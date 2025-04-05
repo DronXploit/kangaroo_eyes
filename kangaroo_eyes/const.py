a7e481bf48724359e5a78e1716559acb60554ede
DronXploit <dronxploit@gmail.com>
2025-04-06 04:36:44 +0700

    Update const.py

--- kangaroo_eyes/const.py
+++ kangaroo_eyes/const.py
@@ -2,7 +2,6 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
-# Color definitions
 COLORS = {
     'info': Fore.CYAN,
     'success': Fore.GREEN,
@@ -13,9 +12,7 @@ COLORS = {
     'border': Fore.LIGHTBLACK_EX
 }
 
-# API Configuration
-API_TIMEOUT = 30  # seconds
-# Perhatikan subdomain yang berbeda
+API_TIMEOUT = 30  
 API_BASE_URL = "https://www.whoisxmlapi.com/whoisapi" 
  
 BANNER = rf"""
